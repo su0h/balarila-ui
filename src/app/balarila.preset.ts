5fb6c1d596454ba6edbcf88a843225f6f857dd21
Andre Ponce <ndr.hbk@gmail.com>
2025-06-22 21:23:08 +0800

    (feature) initial implementation of homepage

--- src/app/balarila.preset.ts
+++ src/app/balarila.preset.ts
@@ -2,66 +2,10 @@
 import { definePreset } from '@primeng/themes';
 import Nora from '@primeng/themes/nora';
 
-const balarilaPrimaryGreen = '#34B86A';
+const balarilaGreen = '#27AE60';
 const balarilaDarkGreen = '#006937';
 
 export const BalarilaPreset = definePreset(Nora, {
     components: {
-        menubar: {
-            background: balarilaPrimaryGreen, 
-            padding: 0,
-            border: {
-                radius: 0,
-                color: 'none'
-            },
-            base: {
-                item: {
-                    padding: '30px', // padding: '30px 10px 30px 10'
-                    border: {
-                        radius: 0,
-                    },
-                }
-            },
-            item: {
-                color: 'white',
-                focus: {
-                    background: 'white',
-                    color: balarilaDarkGreen,
-                },
-                active: {
-                    background: 'white',
-                }
-            },
-            submenu: {
-                icon: {
-                    size: '1000px',
-                    color: 'tomato'
-                }
-            },
-            mobile: {
-                button: {
-                    size: '77px',
-                    color: balarilaDarkGreen,
-                    border: {
-                        radius: 0
-                    },
-                    focus: {
-                        ring: {
-                            offset: '600px 0 0 0',
-                        }  
-                    },
-                }
-            }
-        },
-
-        card: {
-            colorScheme: {
-                light: {
-                    root: {
-                        background: balarilaPrimaryGreen
-                    }
-                }
-            }
-        }
     }
 });
--- src/app/main-layout/homepage/homepage.component.ts
+++ src/app/main-layout/homepage/homepage.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+  
+  constructor(private router: Router) {}
 
+  tryButtonClickHandler() {
+    this.router.navigateByUrl('/grammar-checker');
+  }
 }
